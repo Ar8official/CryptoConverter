383c88cabc37e99b079f8f1525a76b117c586132
Borguuh <abdrahmanadewale@gmail.com>
2021-01-26 00:35:39 +0100

    setting up the API request query

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,3 +1,13 @@
 class CryptoAPI {
-	
+	async getCryptoCurrenciesList() {
+		const url = await fetch('https://api.alternative.me/v1/ticker/', {mode: 'cors'});
+		
+		//return as json
+		const cryptoCurrencies = await url.json();
+		console.log(cryptoCurrencies);
+		//return the object
+		return {
+			cryptoCurrencies
+		}
+	}
 }
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,26 @@
 class UI {
-	
+	constructor(){
+		this.init();
+	}
+	init(){
+		this.printCryptoCurrencies();
+	}
+	//prints the option from the form
+	printCryptoCurrencies(){
+		cryptoAPI.getCryptoCurrenciesList()
+		.then(data => {
+			const cryptoCurrencies = data.cryptoCurrencies;
+			
+			//build the <select> from the REST API
+			const select = document.getElementById('cryptocurrency');
+			
+			cryptoCurrencies.forEach(currency => {
+				//add the <option>
+				const option = document.createElement('option');
+				option.value = currency.id;
+				option.appendChild(document.createTextNode(currency.name));
+				select.appendChild(option);
+			})
+		})
+	}
 }
